a43ffeb76281eab0b9f548096cbd611d30d0837a
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-04-29 15:19:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './index.css';
-
+//test
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store} loading={<PageLoader />}>
     <PersistGate loading={<PageLoader />} persistor={persistor}>
